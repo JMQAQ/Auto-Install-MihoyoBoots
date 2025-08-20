eddf93e2bb40f7de837181aa3ae4a4b3bf66fdb6
1149592618@qq.com <1149592618@qq.com>
2025-08-20 22:56:25 +0800

    添加项目文件。

---
+++ main.c
@@ -0,0 +1,87 @@
+﻿#include <windows.h>  
+#include <wininet.h>  
+#include <stdio.h>  
+
+#pragma comment(lib, "wininet.lib")  
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" ) 
+
+BOOL ClickButton(const char* windowTitle, int x, int y) {
+	HWND hwnd = FindWindowA(NULL, windowTitle);
+	if (hwnd == NULL) {
+		return -1;
+	}
+
+	SetForegroundWindow(hwnd);
+	Sleep(100);
+
+	RECT rect;
+	GetWindowRect(hwnd, &rect);
+
+	int absoluteX = rect.left + x;
+	int absoluteY = rect.top + y;
+
+	SetCursorPos(absoluteX, absoluteY);
+
+	mouse_event(MOUSEEVENTF_LEFTDOWN, absoluteX, absoluteY, 0, 0);
+	Sleep(50);
+	mouse_event(MOUSEEVENTF_LEFTUP, absoluteX, absoluteY, 0, 0);
+
+	return TRUE;
+}
+
+int main() {
+	HINTERNET hInternet, hFile;
+	DWORD bytesRead;
+	char buffer[4096];
+
+	hInternet = InternetOpen("Downloader/1.0", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+	if (hInternet == NULL) {
+		return -1;
+	}
+
+	hFile = InternetOpenUrl(hInternet, "https://ys-api.mihoyo.com/event/download_porter/link/ys_cn/official/pc_backup317", NULL, 0, INTERNET_FLAG_RELOAD, 0);
+	if (hFile == NULL) {
+		InternetCloseHandle(hInternet);
+		return -1;
+	}
+
+	FILE* fp = fopen("1.exe", "wb");
+	if (fp == NULL) {
+		InternetCloseHandle(hFile);
+		InternetCloseHandle(hInternet);
+		return -1;
+	}
+
+	while (InternetReadFile(hFile, buffer, sizeof(buffer) - 1, &bytesRead) && bytesRead > 0) {
+		buffer[bytesRead] = '\0';
+		fwrite(buffer, sizeof(char), bytesRead, fp);
+	}
+
+	fclose(fp);
+	InternetCloseHandle(hFile);
+	InternetCloseHandle(hInternet);
+
+	BlockInput(TRUE);
+
+	ShellExecute(
+		NULL,           
+		"open",         
+		"1.exe",        
+		NULL,           
+		NULL,           
+		SW_RESTORE         
+	);
+
+	for (int i = 0; i < 10; i++) {
+		Sleep(500);
+		HWND hwnd = FindWindow(NULL, "米哈游启动器 安装程序");
+		if (hwnd != NULL) break;
+	}
+
+	ClickButton("米哈游启动器 安装程序", 400, 300);
+	ClickButton("米哈游启动器 安装程序", 500, 300);
+
+
+	BlockInput(FALSE);
+	return 0;
+}
